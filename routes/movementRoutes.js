d0811a9180632809fe0e5f3c229b61f9df4544ce
camilaarevalo1106 <camilaarevalo1106@gmail.com>
2026-05-03 19:23:02 -0500

    cambios

--- routes/movementRoutes.js
+++ routes/movementRoutes.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/movementController");
+// GET TODOS LOS MOVIMIENTOS
+router.get("/", (req, res) => {
+  res.json([
+    {
+      _id: "1",
+      tipo: "ingreso",
+      monto: 1000,
+      categoria: "Salario",
+      descripcion: "Ejemplo"
+    }
+  ]);
+});
 
-router.get("/", controller.getMovements);
-router.post("/", controller.createMovement);
-router.put("/:id", controller.updateMovement);
-router.delete("/:id", controller.deleteMovement);
+// POST
+router.post("/", (req, res) => {
+  res.json({ message: "Movimiento creado", data: req.body });
+});
+
+// PUT
+router.put("/:id", (req, res) => {
+  res.json({ message: "Movimiento actualizado" });
+});
+
+// DELETE
+router.delete("/:id", (req, res) => {
+  res.json({ message: "Movimiento eliminado" });
+});
 
 module.exports = router;
